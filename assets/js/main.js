1e75e437d3345b990b3fb5e04f366f446dedc811
aleshunina <morphia00@gmail.com>
2022-03-26 20:53:02 +0100

    js darkmode search archive logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,31 @@
 const btn = document.getElementById("flexSwitchCheckDefault");
 const archiveBtn = document.getElementById('archiveBtn');
+const searchField = document.getElementById('exampleFormControlInput1');
+const logo = document.getElementById('logo');
+
+function toggleStyle(element, attrib, style1, style2) {
+    if (element.style[attrib] === null) {
+        element.style[attrib] = style1;
+    }
+    if(element.style[attrib] === style1) {
+        element.style[attrib] = style2;
+    } else {
+        element.style[attrib] = style1;
+    }
+  }
+
+function toggleSrc(element, src1, src2) {
+    if(element.src.includes(src1)) {
+        element.src = src2;
+    } else {
+        element.src = src1;
+    }
+}
 
 btn.addEventListener('click', function() {
     document.body.classList.toggle('dark-theme');
-    if (archiveBtn.classList.contains('btn-light')) {
-        archiveBtn.classList.add('btn-dark'); 
-        archiveBtn.classList.remove('btn-light');
-    } else if (archiveBtn.classList.contains('btn-dark')) {
-        archiveBtn.classList.remove('btn-dark');
-        archiveBtn.classList.add('btn-light');
-    }
+    archiveBtn.classList.toggle('btn-dark');
+    toggleStyle(searchField, 'backgroundColor', 'rgb(73, 77, 80)', 'rgb(255, 255, 255)');
+    toggleStyle(searchField, 'color', 'rgb(190, 190, 190)', 'black');
+    toggleSrc(logo, '/assets/img/logo_white.png', '/assets/img/logo_black.png')
 })
