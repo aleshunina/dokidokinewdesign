43f9194ca68763b67598dfe4c3c553c98f0ec32c
anddnov <andr90nov@gmail.com>
2022-04-29 18:39:36 +0300

    change searchResultRadius by js based on order

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,14 @@ $(document).ready(function(){
 logo.addEventListener('click', function() {
     window.location.reload();
 })
+
+
+
+ const searchResult = document.getElementsByClassName('search-result');
+if (searchResult.length == 3){
+    searchResult[0].style.borderBottomLeftRadius = '0px';
+    searchResult[0].style.borderBottomRightRadius = '0px';
+    searchResult[1].style.borderRadius = '0px';
+    searchResult[2].style.borderTopLeftRadius = '0px';
+    searchResult[2].style.borderTopRightRadius = '0px';
+}
